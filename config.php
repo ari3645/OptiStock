a28a8c8d408e064e855dfd7715a7f4146c89a1bd
Albin R. <riviere.albin@gmail.com>
2025-05-23 15:49:40 +0200

    fichier config DB Connexion

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-$host = 'localhost';           // ou adresse IP
-$dbname = 'nom_de_ta_base';    // à adapter
-$username = 'ton_utilisateur'; // à adapter
-$password = 'ton_mot_de_passe';// à adapter
+$host = '192.168.1.87:1433';       
+$dbname = 'VRDM_BDD';    
+$username = 'dev_user'; 
+$password = 'K4bf8Ahb23Jnhy';
 
 try {
     $pdo = new PDO("sqlsrv:Server=$host;Database=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // mode exception
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // fetch par défaut
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
 } catch (PDOException $e) {
     die("❌ Connexion échouée : " . htmlspecialchars($e->getMessage()));
 }
